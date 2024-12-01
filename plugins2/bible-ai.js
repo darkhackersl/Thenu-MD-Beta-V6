ad7954b556479ede5c02b8399862e96cb99da2d0
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-12-01 17:25:42 +0530

    Create bible-ai.js

---
+++ plugins2/bible-ai.js
@@ -0,0 +1,35 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+
+// Define the ai command
+cmd({
+    pattern: "bible",
+    desc: "AI chat",
+    category: "ai",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Fetch response from AI API
+        let data = await fetchJson(`https://api.vihangayt.com/ai/bible?q=${q}`);
+        let response = data.data;
+
+                // Format the reply
+        let replyText = `
+*THENU 𝗠𝗗 BIBLE  𝗥𝗘𝗣𝗟𝗬*
+
+🔍 *𝗤𝘂𝗲𝗿𝘆*: _${q}_
+
+💬 *𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲*: _${response}_
+
+THENUX MD BIBLE INFORMATION`;
+
+        // Send the reply with the thumbnail image
+        await conn.sendMessage(from, { image: { url: 'https://i.ibb.co/mJ5vk7c/Thenu-MD-new-card-1.png' }, caption: replyText }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e.message}`);
+    }
+});
