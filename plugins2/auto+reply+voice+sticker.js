c3bd10b4bd9601fd69d1e9b2ab5a0a84e86ab5a7
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-12-04 13:44:25 +0530

    Create auto+reply+voice+sticker.js

---
+++ plugins2/auto+reply+voice+sticker.js
@@ -0,0 +1,102 @@
+//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
+//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
+//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
+//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
+//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
+//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
+
+
+
+// * Project name - THENU- MD - BETA
+// * Author - Thenula Panpiti
+// * Team - Thenux AI ( One poerson )
+// * Version - V6.0.0
+
+
+// 🧬©Thenu -MD -Beta by Thenux Aiッ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const fs = require('fs');
+const path = require('path');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+//auto_voice
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../Thenu_MD/autovoice.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_VOICE === 'true') {
+                //if (isOwner) return;        
+                await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+            }
+        }
+    }                
+});
+
+//auto sticker 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../Thenu_MD/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'Thenula Panapiti'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
+
+//auto reply 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../Thenu_MD/autoreply.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_REPLY === 'true') {
+                //if (isOwner) return;        
+                await m.reply(data[text])
+            
+            }
+        }
+    }                
+});                
