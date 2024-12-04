8c3eea7d145da7c21cf316f512fadfdad9a63cde
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-12-04 13:49:29 +0530

    Create auto-bio.js

---
+++ plugins2/auto-bio.js
@@ -0,0 +1,189 @@
+//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
+//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
+//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
+//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
+//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
+//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
+
+
+
+// * Project name - THENU- MD - BETA
+// * Author - Thenula Panpiti
+// * Team - Thenux AI ( One poerson )
+// * Version - V6.0.0
+
+
+// 🧬©Thenu -MD -Beta by Thenux Aiッ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x4ebca2 = _0x37f3;
+(function (_0xdb7bd5, _0x578929) {
+    const _0x39c91a = _0x37f3, _0x55851c = _0xdb7bd5();
+    while (!![]) {
+        try {
+            const _0x2475f6 = -parseInt(_0x39c91a(0xfa)) / (-0x88c + 0x50f + 0x37e) + parseInt(_0x39c91a(0x10e)) / (0x61 * 0x49 + 0x6cb + 0x1 * -0x2272) + parseInt(_0x39c91a(0x11b)) / (-0x19 * -0x17e + 0x29 * -0x41 + -0x1 * 0x1ae2) + parseInt(_0x39c91a(0xf1)) / (0x6e * 0x5 + -0x6b9 + 0x5 * 0xeb) * (-parseInt(_0x39c91a(0x108)) / (0x7ef + 0x299 * -0x9 + 0xf77)) + -parseInt(_0x39c91a(0x10b)) / (-0x1e86 + -0xcf7 * -0x2 + 0x49e) * (-parseInt(_0x39c91a(0x118)) / (-0x1343 + 0x6 * -0x1e7 + -0xf * -0x20c)) + parseInt(_0x39c91a(0x105)) / (0x1abb + 0xb65 * 0x2 + 0x7b * -0x67) + -parseInt(_0x39c91a(0xec)) / (-0x255a + 0xd66 + 0x17fd * 0x1) * (parseInt(_0x39c91a(0xf5)) / (-0xf24 + 0x3 * 0x3c2 + 0x32 * 0x14));
+            if (_0x2475f6 === _0x578929)
+                break;
+            else
+                _0x55851c['push'](_0x55851c['shift']());
+        } catch (_0x2ddf71) {
+            _0x55851c['push'](_0x55851c['shift']());
+        }
+    }
+}(_0x11b2, -0x354a3 + -0x1 * -0x3005a + 0x59968));
+const {cmd} = require(_0x4ebca2(0xef)), config = require(_0x4ebca2(0x110));
+let autoBioInterval;
+cmd({
+    'pattern': _0x4ebca2(0xee),
+    'desc': _0x4ebca2(0x100) + _0x4ebca2(0xfc) + _0x4ebca2(0x114) + _0x4ebca2(0x109),
+    'category': _0x4ebca2(0x104),
+    'react': '⚒️',
+    'filename': __filename
+}, async (_0x4fda71, _0x5f5dbb, _0x1b90cc, {
+    from: _0x1bec1b,
+    isOwner: _0xca3f72,
+    reply: _0x40a5fa
+}) => {
+    const _0x5268eb = _0x4ebca2, _0x606abe = {
+            'jMAjZ': function (_0x2b312d, _0x51ecd3) {
+                return _0x2b312d(_0x51ecd3);
+            },
+            'OhUid': _0x5268eb(0xea) + _0x5268eb(0x116) + _0x5268eb(0xf9),
+            'gSDcs': _0x5268eb(0xf6) + _0x5268eb(0x11c) + _0x5268eb(0xfd) + _0x5268eb(0x113),
+            'ObIhW': function (_0x26403d, _0x4048bc) {
+                return _0x26403d(_0x4048bc);
+            },
+            'wGZmw': function (_0xa28c17, _0x572b42) {
+                return _0xa28c17(_0x572b42);
+            },
+            'RYnXt': _0x5268eb(0xf6) + _0x5268eb(0x11c) + _0x5268eb(0xfb) + _0x5268eb(0x107),
+            'ZFhuY': function (_0x405344) {
+                return _0x405344();
+            }
+        };
+    if (!_0xca3f72)
+        return _0x606abe[_0x5268eb(0x119)](_0x40a5fa, _0x606abe[_0x5268eb(0xff)]);
+    config[_0x5268eb(0x10d) + _0x5268eb(0x10c)] = !config[_0x5268eb(0x10d) + _0x5268eb(0x10c)], config[_0x5268eb(0x10d) + _0x5268eb(0x10c)] ? (_0x606abe[_0x5268eb(0x119)](_0x40a5fa, _0x606abe[_0x5268eb(0x101)]), _0x606abe[_0x5268eb(0xf2)](startAutoBio, _0x4fda71)) : (_0x606abe[_0x5268eb(0x115)](_0x40a5fa, _0x606abe[_0x5268eb(0x11a)]), _0x606abe[_0x5268eb(0xf7)](stopAutoBio));
+});
+function startAutoBio(_0x504e85) {
+    const _0xd4b7a8 = _0x4ebca2, _0x43faca = {
+            'cwjtC': function (_0x5996de, _0x2eb0dc) {
+                return _0x5996de(_0x2eb0dc);
+            },
+            'uInOu': function (_0x120a13, _0x2f9c5, _0x49d69d) {
+                return _0x120a13(_0x2f9c5, _0x49d69d);
+            },
+            'pSeDj': function (_0x3f8e3d, _0x6fa113) {
+                return _0x3f8e3d * _0x6fa113;
+            }
+        };
+    if (autoBioInterval)
+        _0x43faca[_0xd4b7a8(0x10f)](clearInterval, autoBioInterval);
+    autoBioInterval = _0x43faca[_0xd4b7a8(0xeb)](setInterval, async () => {
+        const _0x31f371 = _0xd4b7a8, _0x55aed2 = new Date()[_0x31f371(0xed) + _0x31f371(0x102)](), _0x3d8057 = _0x31f371(0xf4) + _0x31f371(0x106) + _0x55aed2 + _0x31f371(0xf3);
+        await _0x504e85[_0x31f371(0xf8) + _0x31f371(0x103)](_0x3d8057);
+    }, _0x43faca[_0xd4b7a8(0x111)](-0xe * 0x28d + -0x171c + 0x3b0e, -0x119 + -0x1ea + 0x7 * 0xfd));
+}
+function stopAutoBio() {
+    const _0x227969 = _0x4ebca2, _0x43d4e1 = {
+            'RrpuL': function (_0x511a17, _0x4035f2) {
+                return _0x511a17(_0x4035f2);
+            },
+            'qvIOg': _0x227969(0xf6) + _0x227969(0x10a) + _0x227969(0x112)
+        };
+    autoBioInterval && (_0x43d4e1[_0x227969(0xf0)](clearInterval, autoBioInterval), autoBioInterval = null, console[_0x227969(0x117)](_0x43d4e1[_0x227969(0xfe)]));
+}
+function _0x37f3(_0x5adea6, _0x5807c2) {
+    const _0x55578c = _0x11b2();
+    return _0x37f3 = function (_0x4092c6, _0x1bad4a) {
+        _0x4092c6 = _0x4092c6 - (0x89d + 0x3a * -0x3d + 0x1 * 0x61f);
+        let _0x137652 = _0x55578c[_0x4092c6];
+        return _0x137652;
+    }, _0x37f3(_0x5adea6, _0x5807c2);
+}
+function _0x11b2() {
+    const _0x5759d4 = [
+        '👨‍💻\x20THENU-MD',
+        '9390bvOgEE',
+        '⚒️\x20AutoBIO\x20',
+        'ZFhuY',
+        'updateProf',
+        'ner!',
+        '372089flbebF',
+        's\x20been\x20*di',
+        'disable\x20th',
+        's\x20been\x20*en',
+        'qvIOg',
+        'OhUid',
+        'Enable\x20or\x20',
+        'gSDcs',
+        'meString',
+        'ileStatus',
+        'owner',
+        '1807296boJEIf',
+        '-BETA\x20[',
+        'sabled*!\x20🚫',
+        '355Cbagfx',
+        'feature.',
+        'feature\x20st',
+        '267222wYwCib',
+        'bled',
+        'autoBioEna',
+        '1306060pFtWeG',
+        'cwjtC',
+        '../config',
+        'pSeDj',
+        'opped.',
+        'abled*!\x20🔄',
+        'e\x20AutoBIO\x20',
+        'wGZmw',
+        'not\x20the\x20ow',
+        'log',
+        '28BJteCu',
+        'jMAjZ',
+        'RYnXt',
+        '1296192pEkfhE',
+        'feature\x20ha',
+        '❌\x20You\x20are\x20',
+        'uInOu',
+        '3789HFrKfS',
+        'toLocaleTi',
+        'setautobio',
+        '../command',
+        'RrpuL',
+        '21204Sjydfm',
+        'ObIhW',
+        ']\x20👨‍💻'
+    ];
+    _0x11b2 = function () {
+        return _0x5759d4;
+    };
+    return _0x11b2();
+}
